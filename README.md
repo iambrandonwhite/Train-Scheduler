361042958ef6d0734b73f3438a2b4ff8af4941bd
Brandon White <46477427+iambrandonwhite@users.noreply.github.com>
2019-03-16 20:06:12 -0700

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Train-Scheduler
+This is the seventh assignment for the UCR Coding Bootcamp.
+
+In this assignment, we were required to create a firebase database & create a webpasge that can read and write to the database.
+
+Instructions: Input all info fields to create a train schedule.
+
+#Built With
+* [HTML](https://html.com/) - HTML
+* [CSS](https://www.w3schools.com/css/default.asp) - CSS
+* [Jquery](https://jquery.com/) - JQuery
+* [Javascript](https://www.javascript.com/) - Javascript
+* [Moment.js](https://momentjs.com/) - Moment.js
+
+#Author
+Brandon White - https://github.com/iambrandonwhite
+
+#Check out the site in the link below!
+https://iambrandonwhite.github.io/Train-Scheduler/
---
+++ assets/javascript/app.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBW-p4LOUKB9ZQR2ouFbcMzKhsJmsLTQ3E",
+    authDomain: "train-scheduler-898df.firebaseapp.com",
+    databaseURL: "https://train-scheduler-898df.firebaseio.com",
+    projectId: "train-scheduler-898df",
+    storageBucket: "train-scheduler-898df.appspot.com",
+    messagingSenderId: "573333044777"
+  };
+  // Get databse
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  // Initial Values
+  var name;
+  var destination;
+  var firstTrain;
+  var frequency = 0;
+
+  // Capture Button Click
+  $("#submitBtn").on("click", function () {
+      event.preventDefault();
+
+      // Storing and retreiving most recent train data
+      name = $("#trainNameInput").val().trim();
+      destination = $("#destinationInput").val().trim();
+      firstTrain = $("#firstTrainTimeInput").val().trim();
+      frequency = $("#frequencyInput").val().trim();
+
+      // Code for the push to database
+      database.ref().push({
+          name: name,
+          destination: destination,
+          firstTrain: firstTrain,
+          frequency: frequency,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+      // Reset form
+      $("form")[0].reset();
+  });
+
+  // Firebase watcher + initial loader
+  database.ref().on("child_added", function (childSnapshot) {
+      var minsAway;
+      // Subtract 1 year as firstTrain comes before now making the new firstTrain
+      var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+      // Difference between the current and firstTrain
+      var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+      var remainder = diffTime % childSnapshot.val().frequency;
+      // Minutes until next train
+      var minsAway = childSnapshot.val().frequency - remainder;
+      // Calculate next train time
+      var nextTrain = moment().add(minsAway, "minutes");
+      nextTrain = moment(nextTrain).format("hh:mm");
+
+      $("#addTrain").append("<tr><td>" + childSnapshot.val().name +
+          "</td><td>" + childSnapshot.val().destination +
+          "</td><td>" + childSnapshot.val().frequency +
+          "</td><td>" + nextTrain +
+          "</td><td>" + minsAway + "</td></tr>");
+
+      // Handle the errors
+  }, function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+  });
+});
